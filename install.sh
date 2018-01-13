981714578a1996eeecdcf02609ddccf67841a128
Alex Ellwein <alex.ellwein@gmail.com>
2018-01-13 12:00:11 +0100

    clean up after installation

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 cwd="$(pwd)"
 cd /tmp
 git clone https://aur.archlinux.org/package-query.git
+git clone https://aur.archlinux.org/yaourt.git
 cd package-query
 makepkg -si
-cd ..
-git clone https://aur.archlinux.org/yaourt.git
-cd yaourt
+cd ../yaourt
 makepkg -si
+cd ..
+rm install.sh
 cd ${cwd}
 
